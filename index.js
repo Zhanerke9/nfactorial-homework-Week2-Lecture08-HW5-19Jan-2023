80aaa92ceb7dd8a6fb0a0c9ff78c5112a8283853
Zhanerke9 <zhanerke.sauranbay@gmail.com>
2023-01-19 13:39:29 +0500

    nfactorial-homework-Week2-Lecture08-HW5-19Jan-2023

---
+++ index.js
@@ -0,0 +1,30 @@
+//work in https://learn.javascript.ru/task/find-elements/table.html
+// screenshot attached
+// Как найти?…
+
+// // 1. Таблицу с `id="age-table"`.
+// var ageTable = document.getElementById("age-table");
+// console.log(ageTable);
+
+// // 2. Все элементы `label` внутри этой таблицы (их три).
+// var labels = ageTable.getElementsByTagName("label");
+// console.log(labels);
+
+// // 3. Первый `td` в этой таблице (со словом «Age»).
+// var firstTd = ageTable.getElementsByTagName("td")[0];
+// console.log(firstTd);
+
+// // 4. Форму `form` с именем `name="search"`.
+// var searchForm = document.forms.search;
+// console.log(searchForm);
+
+// // 5. Первый `input` в этой форме.
+// var firstInput = searchForm.getElementsByTagName("input")[0];
+// console.log(firstInput);
+
+// // 6. Последний `input` в этой форме.
+// var lastInput = searchForm.getElementsByTagName("input")[searchForm.getElementsByTagName("input").length - 1];
+// console.log(lastInput);
+
+
+//second ex - смотреть в index.html
